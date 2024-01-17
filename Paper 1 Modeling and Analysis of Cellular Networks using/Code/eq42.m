22654eebb13d0e32cc78b9f295dd73f61522ddc9
M. Bouzid <74613831+medbouzid@users.noreply.github.com>
2024-01-17 02:50:06 -0500

    Delete Paper 1 Modeling and Analysis of Cellular Networks using directory

--- Paper 1 Modeling and Analysis of Cellular Networks using/Code/eq42.m
+++
@@ -1,9 +0,0 @@
-function E = eq42(lambda)
-% calculate the outage probability
-P=10;
-N0=1e-11;
-E=[];
-for r0=10:10:1000
-    fun=@(t) exp(-t.*N0.*r0.^4./P-pi.*lambda.*sqrt(t).*r0^2.*atan(sqrt(t)))./(t+1);
-    E=[E,integral(fun,0,inf)];
-end
--- Paper 1 Modeling and Analysis of Cellular Networks using/Code/eq43.m
+++
@@ -1,5 +0,0 @@
-function O = eq43(lambda,T,r0)
-P=10;
-N0=1e-11;
-O=1-exp(-T.*N0.*r0.^4./P-pi.*lambda.*sqrt(T).*r0.^2.*atan(sqrt(T))); %Calculate outage probability
-end
